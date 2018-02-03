c9fdc150c6c563e09d4e77e88c83335be4516726
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-02-03 21:30:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ return `I can\'t hear you!`
 }
 
 function sayHiToGrandma(string) {
-  if (string.toUpperCase()) {
+  if (string.toUpperCase()=== string) {
     return "YES INDEED!";
-  } else if(string.toLowerCase) {
+  } else if(string.toLowerCase()=== string) {
     return "I can\'t hear you!";
   }
 }
